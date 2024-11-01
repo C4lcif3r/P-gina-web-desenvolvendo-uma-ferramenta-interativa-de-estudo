f6abee6f78213933c4ef0bc42870fd072d67941b
calcifer <00001081546013sp@al.educacao.sp.gov.br>
2024-11-01 13:39:01 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vôlei',
+    'Quantos jogadores cada time tem em quadra?',
+    'Cada time tem seis jogadores em quadra'
+)
+
+criaCartao(
+    'Vôlei',
+    'Qual é a altura da rede no vôlei masculino?',
+    'A altura da rede no vôlei masculino é 2,43 metros'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que é um saque no vôlei?',
+    'O saque é a ação de iniciar a jogada enviando a bola para o lado adversário'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que significa "set" no vôlei?',
+    'Um set é uma parte do jogo, e a equipe que vence três sets ganha a partida'
+)
